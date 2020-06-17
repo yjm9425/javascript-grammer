d3ae221887c6906350466f08980689a424e85451
염정민 <yjm9425@gmail.com>
2020-06-17 23:11:01 +0900

    array.join() 정의

---
+++ quiz/array.js
@@ -0,0 +1,7 @@
+// 배열을 문자열로 만들기
+{
+  const fruits = ["아이파크", "코오롱 하늘채", "래미안"];
+  // join() 메서드는 배열내 요소를 연결하여 문자열을 만듭니다.
+  const result = fruits.join(",");
+  console.log(result); // 아이파크,코오롱 하늘채,래미안
+}
