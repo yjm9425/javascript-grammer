0ffee9068fb5e4c50fa5caa2bba1d31e613dd296
염정민 <yjm9425@gmail.com>
2020-06-18 22:35:59 +0900

    array.split()  정의

--- quiz/array.js
+++ quiz/array.js
@@ -5,3 +5,10 @@
   const result = fruits.join(",");
   console.log(result); // 아이파크,코오롱 하늘채,래미안
 }
+
+{
+  const station = "상월곡,월곡,돌곶이,보문,고려대";
+  // split() 메서드는 문자열을 지정된 구분자를 기준으로 각각의 요소로 분류하여 배열로 만듭니다.
+  const stationsArr = station.split(",");
+  console.log(stationsArr);
+}
